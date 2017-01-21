e92649d0e84273a0f51ac9637429f758058f8ebb
lexiht <hazukitran@gmail.com>
2017-01-21 16:59:08 +0000

    WIP Controller test

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,11 +2,10 @@ require 'rover'
 
 class Controller
 
-  attr_reader :input, :results
+  attr_reader :input
 
- def initialize(input, rover = Rover)
+ def initialize(input = Input.new)
    @input = input.rovers_instructions
-   @rover = rover
    @results = []
  end
 
@@ -14,8 +13,9 @@ class Controller
    a = 0
    input.count.times do |i|
      rover = Rover.new(input[a][0], input[a][1])
-     @results << @rover.follow_instruction
+     @results << rover.follow_instruction
      a += 1
    end
+   @results
  end
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -2,27 +2,27 @@ require 'controller'
 require 'input'
 
 describe Controller do
-  subject(:controller) { described_class.new(input, rover)}
+  subject(:controller) { described_class.new(input)}
   let(:input) { double :input, rovers_instructions: instructions }
-  let(:rover) { double :rover }
   let(:instructions) { [[[1, 2, 'N'], ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M']],
                         [[3, 3, 'E'], ['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M']]] }
   let(:results) { ['1 3 N', '5 1 E'] }
 
   describe '#activate_rovers' do
+    before(:each) do
+      class_double('Rover').as_stubbed_const(follow_instruction: "", new: Rover)
+      # allow_any_instance_of(Rover).to receive(:follow_instruction).and_return("")
+    end
 
     it 'instantiates instances of rover' do
-      class_double('Rover', :follow_instruction).as_stubbed_const
-      Rover.any_stance.stub(:follow_instruction)
       expect(Rover).to receive(:new).with(instructions[0][0], instructions[0][1])
       expect(Rover).to receive(:new).with(instructions[1][0], instructions[1][1])
       controller.activate_rovers
     end
 
     it 'returns the final coordinate from all the rovers' do
-      class_double('Rover', :follow_instruction, :new).as_stubbed_const
       controller.activate_rovers
-      expect(controller.results).to eq(results)
+      expect(controller.activate_rovers).to eq(results)
     end
 
   end
