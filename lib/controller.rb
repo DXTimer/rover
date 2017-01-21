e9983be3d76f95e08f720e05ac3ab9ec8808149c
lexiht <hazukitran@gmail.com>
2017-01-21 16:43:51 +0000

    WIP unit test Controller class

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,21 @@
+require 'rover'
+
 class Controller
 
+  attr_reader :input, :results
+
+ def initialize(input, rover = Rover)
+   @input = input.rovers_instructions
+   @rover = rover
+   @results = []
+ end
+
+ def activate_rovers
+   a = 0
+   input.count.times do |i|
+     rover = Rover.new(input[a][0], input[a][1])
+     @results << @rover.follow_instruction
+     a += 1
+   end
+ end
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,19 +1,29 @@
-# require 'controller'
-# require 'input'
-#
-# describe Controller do
-#   subject(:controller) { described_class.new(input, rover)}
-#   # let(:input) { Input.new('./spec/fixtures/test_input.txt') }
-#   let(:input) { double :input }
-#   let(:rover) { double :rover }
-#   # let(:instructions) { [['1 2 N', 'LMLMLMLMM'], ['3 3 E', 'MMRMMRMRRM']] }
-#
-#   describe '#activate_rover' do
-#     it 'connects the rovers' do
-#       allow(input).to receive(:area_coordinate)
-#       allow(input).to receive(:rovers_instructions)
-#       number_of_rovers = input.rovers_instructions.count
-#       expect()
-#     end
-#   end
-# end
+require 'controller'
+require 'input'
+
+describe Controller do
+  subject(:controller) { described_class.new(input, rover)}
+  let(:input) { double :input, rovers_instructions: instructions }
+  let(:rover) { double :rover }
+  let(:instructions) { [[[1, 2, 'N'], ['L', 'M', 'L', 'M', 'L', 'M', 'L', 'M', 'M']],
+                        [[3, 3, 'E'], ['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M']]] }
+  let(:results) { ['1 3 N', '5 1 E'] }
+
+  describe '#activate_rovers' do
+
+    it 'instantiates instances of rover' do
+      class_double('Rover', :follow_instruction).as_stubbed_const
+      Rover.any_stance.stub(:follow_instruction)
+      expect(Rover).to receive(:new).with(instructions[0][0], instructions[0][1])
+      expect(Rover).to receive(:new).with(instructions[1][0], instructions[1][1])
+      controller.activate_rovers
+    end
+
+    it 'returns the final coordinate from all the rovers' do
+      class_double('Rover', :follow_instruction, :new).as_stubbed_const
+      controller.activate_rovers
+      expect(controller.results).to eq(results)
+    end
+
+  end
+end
